8581f3663c0ac9b7d71b6ef6169813240e13b641
chunminchang <cchang@mozilla.com>
2016-07-12 13:11:18 +0800

    Delay the initializatin of remote-control service

--- b2g/chrome/content/remote_control.js
+++ b2g/chrome/content/remote_control.js
@@ -8,69 +8,102 @@
 
 Cu.import("resource://gre/modules/Services.jsm");
 
+function getCurrentTime() {
+  let time = new Date().getTime();
+  let str = new Date(time).toString();
+  return str;
+}
+
+function debug(s) {
+  dump("[ " + getCurrentTime() + " ] remote_control: " + s + "\n");
+}
+
 const kPrefPresentationDiscoverable = "dom.presentation.discoverable";
 
-(function setupRemoteControlSettings() {
-  // To keep RemoteContorlService in the scope to prevent import again
-  var remoteControlScope = {};
+// To keep RemoteContorlService in the scope to prevent import again
+var remoteControlScope = {};
 
-  function importRemoteControlService() {
-    if (!("RemoteControlService" in remoteControlScope)) {
-      Cu.import("resource://gre/modules/RemoteControlService.jsm", remoteControlScope);
-    }
+function importRemoteControlService() {
+  if (!("RemoteControlService" in remoteControlScope)) {
+    Cu.import("resource://gre/modules/RemoteControlService.jsm", remoteControlScope);
   }
+}
 
-  function setupRemoteControl() {
-    if (Services.prefs.getBoolPref(kPrefPresentationDiscoverable)) {
+var statusMonitor = {
+  init: function sm_init() {
+    debug("init");
+
+    Services.prefs.addObserver(kPrefPresentationDiscoverable, this, false);
+    Services.obs.addObserver(this, "network-active-changed", false);
+    Services.obs.addObserver(this, "network:offline-status-changed", false);
+  },
+
+  get discoverable() {
+    return Services.prefs.getBoolPref(kPrefPresentationDiscoverable);
+  },
+
+  observe: function sm_observe(subject, topic, data) {
+    debug("observe: " + topic + ", " + data);
+
+    switch (topic) {
+      case "network-active-changed": {
+        if (!subject) {
+          // Stop service when there is no active network
+          this.stopService();
+          break;
+        }
+
+        // Start service when active network change with new IP address
+        // Other case will be handled by "network:offline-status-changed"
+        this.startService();
+        break;
+
+        break;
+      }
+      case "network:offline-status-changed": {
+        if (data == "offline") {
+          // Stop service when network status change to offline
+          this.stopService();
+        } else { // online
+          // Resume service when network status change to online
+          this.startService();
+        }
+        break;
+      }
+      case "nsPref:changed": {
+        if (data != kPrefPresentationDiscoverable) {
+          break;
+        }
+
+        if (!this.startService()) {
+          this.stopService();
+        }
+
+        break;
+      }
+      default:
+        break;
+    }
+  },
+
+  startService: function sm_startService() {
+    debug("startService");
+    // Start only when network is alive and presentation is discoverable
+    if (!Services.io.offline && this.discoverable) {
       importRemoteControlService();
       remoteControlScope.RemoteControlService.start();
-    } else {
-      remoteControlScope.RemoteControlService.stop();
+      debug("  YES");
+      return true;
     }
+    debug("  NO");
+    return false;
+  },
+
+  stopService: function sm_stopService() {
+    debug("stopService");
+    remoteControlScope.RemoteControlService &&
+    remoteControlScope.RemoteControlService.stop();
   }
+};
 
-  Services.prefs.addObserver(kPrefPresentationDiscoverable, setupRemoteControl, false);
-
-  var networkStatusMonitor = {
-    // nsIObserver
-    observe: function(subject, topic, data) {
-      switch (topic) {
-        case "network-active-changed": {
-          if (!subject) {
-            // Stop service when there is no active network
-            remoteControlScope.RemoteControlService.stop();
-            break;
-          }
-
-          // Start service when active network change with new IP address
-          // Other case will be handled by "network:offline-status-changed"
-          if (!Services.io.offline && Services.prefs.getBoolPref(kPrefPresentationDiscoverable)) {
-            importRemoteControlService();
-            remoteControlScope.RemoteControlService.start();
-          }
-          break;
-        }
-        case "network:offline-status-changed": {
-          if (data == "offline") {
-            // Stop service when network status change to offline
-            remoteControlScope.RemoteControlService.stop();
-          } else if (Services.prefs.getBoolPref(kPrefPresentationDiscoverable)) {
-            // Resume service when network status change to online
-            importRemoteControlService();
-            remoteControlScope.RemoteControlService.start();
-          }
-          break;
-        }
-        default:
-          break;
-      }
-    }
-  };
-
-  if (Ci.nsINetworkManager) {
-    Services.obs.addObserver(networkStatusMonitor, "network-active-changed", false);
-    Services.obs.addObserver(networkStatusMonitor, "network:offline-status-changed", false);
-  }
-
-  setupRemoteControl();
-})();
+statusMonitor.init();
