19ebe1c7e302b7aaaf99892efa3119b2d05c9e54
Chun-Min Chang <chun.m.chang@gmail.com>
2017-02-11 18:36:01 +0800

    Bug 1338012 - Enable multiple channels on OS X; r?jya
    MozReview-Commit-ID: 3NIt7JNmgTJ

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -90,7 +90,7 @@ private:
   DECL_MEDIA_PREF("accessibility.monoaudio.enable",           MonoAudio, bool, false);
   DECL_MEDIA_PREF("media.resampling.enabled",                 AudioSinkResampling, bool, false);
   DECL_MEDIA_PREF("media.resampling.rate",                    AudioSinkResampleRate, uint32_t, 48000);
-#if defined(XP_WIN) || defined(LINUX)
+#if defined(XP_WIN) || defined(LINUX) || defined(XP_MACOSX)
   // Enable multiple channel support on Windows.
   DECL_MEDIA_PREF("media.forcestereo.enabled",                AudioSinkForceStereo, bool, false);
 #else
