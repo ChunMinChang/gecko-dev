a00e3423bb907e57433475191d0cd923ca78f3d3
Chun-Min Chang <chun.m.chang@gmail.com>
2023-11-28 09:17:06 -0800

    Bug 1860899 - Allocate/Free AVCodecContext in Init()/Shutdown()
    This patch partially implements Init() and Shutdown() to allocate and
    release the AVCodecContext respectively. These two functions will be
    exectued on the platform's encoder task queue specified in the given
    CreateEncoderParams. Thus, the allocated AVCodecContext is a task-queue
    only member variable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194580

--- dom/media/platforms/ffmpeg/FFmpegVideoEncoder.h
+++ dom/media/platforms/ffmpeg/FFmpegVideoEncoder.h
@@ -24,11 +24,11 @@ class FFmpegVideoEncoder {};
 template <>
 class FFmpegVideoEncoder<LIBAV_VER> final : public MediaDataEncoder {
  public:
-  explicit FFmpegVideoEncoder(const FFmpegLibWrapper* aLib) : mLib(aLib) {
-    MOZ_ASSERT(mLib);
-  };
+  FFmpegVideoEncoder(const FFmpegLibWrapper* aLib, AVCodecID aCodecID,
+                     RefPtr<TaskQueue> aTaskQueue);
 
   /* MediaDataEncoder Methods */
+  // All methods run on the task queue, except for GetDescriptionName.
   RefPtr<InitPromise> Init() override;
   RefPtr<EncodePromise> Encode(const MediaData* aSample) override;
   RefPtr<EncodePromise> Drain() override;
@@ -39,8 +39,16 @@ class FFmpegVideoEncoder<LIBAV_VER> final : public MediaDataEncoder {
  private:
   ~FFmpegVideoEncoder() = default;
 
+  RefPtr<InitPromise> ProcessInit();
+  void ProcessShutdown();
+
   // This refers to a static FFmpegLibWrapper, so raw pointer is adequate.
-  const FFmpegLibWrapper* mLib;  // set in constructor
+  const FFmpegLibWrapper* mLib;        // set in constructor
+  const AVCodecID mCodecID;            // set in constructor
+  const RefPtr<TaskQueue> mTaskQueue;  // set in constructor
+
+  // mTaskQueue only.
+  AVCodecContext* mCodecContext;
 };
 
 }  // namespace mozilla
