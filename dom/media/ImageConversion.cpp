b5120886e487e6ae35f958ecdf02a522d3016c94
Chun-Min Chang <chun.m.chang@gmail.com>
2024-04-22 17:43:15 -0700

    Rename ImageToI420 to ImageConversion

--- dom/media/ImageConversion.cpp
+++ dom/media/ImageConversion.cpp
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "ImageToI420.h"
+#include "ImageConversion.h"
 
 #include "ImageContainer.h"
 #include "libyuv/convert.h"
