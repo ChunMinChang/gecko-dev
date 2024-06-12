1b99e7503e6975080e5ab391461019f16338c5e8
Chun-Min Chang <chun.m.chang@gmail.com>
2024-06-11 17:09:01 -0700

    Bug 1865896 - Reorder headers in FFVPXRuntimeLinker.cpp by coding style
    Differential Revision: https://phabricator.services.mozilla.com/D204450
    
    Depends on D204449

--- dom/media/platforms/ffmpeg/ffvpx/FFVPXRuntimeLinker.cpp
+++ dom/media/platforms/ffmpeg/ffvpx/FFVPXRuntimeLinker.cpp
@@ -5,6 +5,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "FFVPXRuntimeLinker.h"
+
+#ifdef XP_WIN
+#  include <windows.h>
+#endif
+
 #include "FFmpegLibWrapper.h"
 #include "FFmpegLog.h"
 #include "mozilla/FileUtils.h"
